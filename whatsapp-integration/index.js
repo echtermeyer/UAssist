34f0403fb2bcb839d199c9df90d58e96da6e30e3
I550854 <eric.echtermeyer@sap.com>
2026-05-12 12:48:40 +0200

    no sandbox

--- whatsapp-integration/index.js
+++ whatsapp-integration/index.js
@@ -1,7 +1,10 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client({ authStrategy: new LocalAuth() });
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+});
 
 client.on('qr', qr => qrcode.generate(qr, { small: true }));
 
