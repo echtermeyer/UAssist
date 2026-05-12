af9253e2b452da69e1146fda11377ab65fd981c0
I550854 <eric.echtermeyer@sap.com>
2026-05-12 12:37:28 +0200

    first commit

---
+++ whatsapp-integration/index.js
@@ -0,0 +1,16 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({ authStrategy: new LocalAuth() });
+
+client.on('qr', qr => qrcode.generate(qr, { small: true }));
+
+client.on('ready', () => console.log('✅ Connected!'));
+
+client.on('message', async msg => {
+    const chat = await msg.getChat();
+    console.log(`[${chat.name}] ${msg.body}`);
+    console.log(msg);
+});
+
+client.initialize();
